dfc9c392756ba67763195e69b6494ee782584f7b
jibon8161 <jibon2230@gmail.com>
2024-02-13 19:20:31 +0600

    promotion

--- src/carousel/Slide.js
+++ src/carousel/Slide.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { CiDeliveryTruck } from "react-icons/ci";
+import { GiLindenLeaf } from "react-icons/gi";
+import { PiDotsThreeVerticalBold } from "react-icons/pi";
+import { CiDollar } from "react-icons/ci";
+import { AiFillSafetyCertificate } from "react-icons/ai";
 const Slide = () => {
   return (
     <div>
@@ -40,8 +44,69 @@ const Slide = () => {
               3
             </a>
           </div>
-          <div className="absolute bottom-16 ">
-           
+          <div className="absolute bottom-52 ml-12 ">
+            <div className="flex border  shadow-md p-4  ">
+              <div className="flex gap-2 hover:text-green-600 ">
+                <p className="text-6xl font-thin rounded-full shadow-inner">
+                  <CiDeliveryTruck />
+                </p>
+                <div className="text-left">
+                  <p className="font-bold">Free Shipping</p>
+                  <p className="text-xs">
+                    Free shipping when you spend <br /> $1000.
+                  </p>
+                </div>
+              </div>
+              <div className="text-6xl text-gray-400">
+                {/* <RxDividerVertical /> */}
+                <PiDotsThreeVerticalBold />
+              </div>
+              <div className="flex gap-2 hover:text-green-600 ">
+                <p className="text-6xl font-thin rounded-full shadow-inner">
+                  <GiLindenLeaf />
+                </p>
+                <div className="text-left">
+                  <p className="font-bold">Get Fresh Products</p>
+                  <p className="text-xs">
+                    Find a range of best online <br /> premium products.
+                  </p>
+                </div>
+              </div>
+              <div className="text-6xl text-gray-400">
+                {/* <RxDividerVertical /> */}
+                <PiDotsThreeVerticalBold />
+              </div>
+              <div className="flex gap-2 hover:text-green-600 ">
+                <p className="text-6xl font-thin rounded-full shadow-inner">
+                  <CiDollar />
+                </p>
+                <div className="text-left">
+                  <p className="font-bold">Moneyback Offer</p>
+                  <p className="text-xs">
+                    Free 30 days money back <br /> guarantee.
+                  </p>
+                </div>
+              </div>
+              <div className="text-6xl text-gray-400">
+                {/* <RxDividerVertical /> */}
+                <PiDotsThreeVerticalBold />
+              </div>
+              <div className="flex gap-2 hover:text-green-600 ">
+                <p className="text-6xl font-thin rounded-full shadow-inner">
+                  <AiFillSafetyCertificate />
+                </p>
+                <div className="text-left">
+                  <p className="font-bold">Safe Payment</p>
+                  <p className="text-xs">
+                    We are using secure payment <br /> methods
+                  </p>
+                </div>
+              </div>
+              <div className="text-6xl text-gray-400">
+                {/* <RxDividerVertical /> */}
+                <PiDotsThreeVerticalBold />
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-span-2"></div>
